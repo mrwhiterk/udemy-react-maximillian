c3f96fdf24df45125fd74649a4b97f09f8ac009b
Ryan White <mrwhiterk@gmail.com>
2019-12-03 14:50:22 -0500

    got total price showing on modal with cancel and continue button

--- burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey, i) => {
     return (
-      <li key={igKey+i}>
+      <li key={igKey + i}>
         <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
         {props.ingredients[igKey]}
       </li>
@@ -16,12 +17,17 @@ const orderSummary = props => {
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)} </strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   )
 }
