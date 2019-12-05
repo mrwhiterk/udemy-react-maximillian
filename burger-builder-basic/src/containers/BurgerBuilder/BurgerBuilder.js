0e95339e26417073c4daf4b49831d7c3f6461153
Ryan White <mrwhiterk@gmail.com>
2019-12-04 23:48:40 -0500

    successfully sending post request to firebase

--- burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!')
+    // alert('You continue!')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice
+      , customer: {
+        name: 'Ryan White',
+        address: {
+          street: '123 test st',
+          zipCode: '41351',
+          country: 'Germany'
+        },
+        email: 'test@test.com'
+      },
+      deliverMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   }
 
   render() {
