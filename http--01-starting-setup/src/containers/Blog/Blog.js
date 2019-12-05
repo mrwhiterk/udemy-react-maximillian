d0814a78d94a55827845058b0b0cb570aa17c690
Ryan White <mrwhiterk@gmail.com>
2019-12-05 12:11:44 -0500

    started refactoring components to container that will be used with routing

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 // import axios from 'axios'
 import axios from '../../axios'
-import Post from '../../components/Post/Post'
-import FullPost from '../../components/FullPost/FullPost'
-import NewPost from '../../components/NewPost/NewPost'
+
+
 import './Blog.css'
 
 class Blog extends Component {
@@ -65,12 +64,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     )
   }
