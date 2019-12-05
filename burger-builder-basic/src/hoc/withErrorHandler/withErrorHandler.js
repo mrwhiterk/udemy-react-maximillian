e5c5b7fb222af9eb403416718628449aa7cbbcd9
Ryan White <mrwhiterk@gmail.com>
2019-12-05 02:03:53 -0500

    got componentWillMount warning to dismiss

--- burger-builder-basic/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder-basic/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Modal from '../../components/UI/Modal/Modal'
 import Aux from '../Aux/Aux'
-// import axios from 'axios'
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
@@ -9,15 +8,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     }
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
       axios.interceptors.request.use(req => {
         this.setState({ error: null })
-        return req;
+        return req
       })
 
-      axios.interceptors.response.use(res => res, err => {
-        this.setState({ error: err })
-      })
+      axios.interceptors.response.use(
+        res => res,
+        err => {
+          this.setState({ error: err })
+        }
+      )
     }
 
     errorConfirmedHandler = () => {
@@ -27,7 +29,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
     render() {
       return (
         <Aux>
-          <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>{this.state.error ? this.state.error.message : null}</Modal>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
           <WrappedComponent {...this.props} />
         </Aux>
       )
