b117775e6704249e0b8478d60c6243c70adc3ff3
Ryan White <mrwhiterk@gmail.com>
2019-12-04 22:19:01 -0500

    created axios interceptor to read and/or manipulate any request to/from react app

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log(request)
+  // edit request config
+  return request;
+}, err => {
+    console.log(err)
+    return Promise.reject(err)
+})
+
+axios.interceptors.response.use(
+  response => {
+    console.log(response)
+    // edit request config
+    return response
+  },
+  err => {
+    console.log(err)
+    return Promise.reject(err)
+  }
+)
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
