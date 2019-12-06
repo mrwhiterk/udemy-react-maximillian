f35ea7f2fed6fb807aa11c33d3d67bef153b7616
Ryan White <mrwhiterk@gmail.com>
2019-12-06 15:02:33 -0500

    setup guard to protect route from unauthenticated user

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -8,6 +8,10 @@ import NewPosts from './NewPost/NewPost'
 // import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
+  state = {
+    auth: false
+  }
+
   render() {
     return (
       <div className="Blog">
@@ -45,7 +49,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
         <Route path="/" render={() => <h1>Home 2</h1>}/> */}
         <Switch>
-          <Route path="/new-post" component={NewPosts} />
+          {this.state.auth ? <Route path="/new-post" component={NewPosts} /> : null}
           <Route path="/posts" component={Posts} />
           <Redirect from='/' to="/posts" />
           {/* <Route path="/" component={Posts} /> */}
