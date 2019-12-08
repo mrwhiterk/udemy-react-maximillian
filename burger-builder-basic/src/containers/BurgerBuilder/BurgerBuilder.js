96151caae93cccc367bfb0f5d45ec1725daa5ec7
Ryan White <mrwhiterk@gmail.com>
2019-12-07 23:10:36 -0500

    moved axios code to ContactData component to work with React router and submit on 'order' btn click

--- burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,31 +87,14 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!')
-    // this.setState({ loading: true })
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Ryan White',
-    //     address: {
-    //       street: '123 test st',
-    //       zipCode: '41351',
-    //       country: 'Germany'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliverMethod: 'fastest'
-    // }
-
-    // axios
-    //   .post('/orders.json', order)
-    //   .then(res => this.setState({ loading: false, purchasing: false }))
-    //   .catch(err => this.setState({ loading: false, purchasing: false }))
+    
     const queryParams = [];
 
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+
+    queryParams.push('price=' + this.state.totalPrice)
 
     const queryString = queryParams.join('&');
 
--- burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
+import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
+import axios from '../../../axios-orders'
 
 class ContactData extends Component {
   state = {
@@ -9,23 +11,80 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = event => {
+    event.preventDefault()
+    this.setState({ loading: true })
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Ryan White',
+        address: {
+          street: '123 test st',
+          zipCode: '41351',
+          country: 'Germany'
+        },
+        email: 'test@test.com'
+      },
+      deliverMethod: 'fastest'
     }
+
+    axios
+      .post('/orders.json', order)
+      .then(res => {
+        this.setState({ loading: false })
+        this.props.history.push('/');
+      })
+      .catch(err => this.setState({ loading: false }))
   }
 
   render() {
+    let form = (
+      <form>
+        <input
+          className={classes.Input}
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+        <input
+          className={classes.Input}
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="postal"
+          placeholder="Postal Code"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    )
+    if (this.state.loading) {
+      form = <Spinner />
+    }
+
     return (
-      <div className={classes.ContactData} >
+      <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success">ORDER</Button>
-        </form>
+        {form}
       </div>
     )
   }
 }
 
-export default ContactData;
+export default ContactData
