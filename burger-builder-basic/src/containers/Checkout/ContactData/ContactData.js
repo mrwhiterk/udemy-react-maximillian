3c3c453d08c294b190396fb90b09cb0b28f49b53
Ryan White <mrwhiterk@gmail.com>
2019-12-20 20:41:46 -0500

    fixed bug of dropdown not submitting default value in contact form

--- burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
@@ -89,7 +89,8 @@ class ContactData extends Component {
             }
           ]
         },
-        value: '',
+        value: 'fastest',
+        validation: {},
         valid: true
       }
     },
