be70a1412c6f0603a4104f56f9d8c9aab0896a52
Ryan White <mrwhiterk@gmail.com>
2019-12-29 19:02:54 -0500

    updated form submit to enable using new redux ingredient function

--- burger-builder-basic/src/store/reducer.js
+++ burger-builder-basic/src/store/reducer.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions';
+import * as actionTypes from './actions'
 
 const initialState = {
   ingredients: {
@@ -10,16 +10,23 @@ const initialState = {
   totalPrice: 4
 }
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT: 
+    case actionTypes.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        }
-
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       }
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -27,13 +34,12 @@ const reducer = (state = initialState, action) => {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        }
-
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       }
     default:
-      return state;
+      return state
   }
+}
 
-} 
-
-export default reducer;
+export default reducer
