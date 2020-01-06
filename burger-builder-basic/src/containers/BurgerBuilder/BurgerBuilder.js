4fc3afe5fdd56d449ec88cb3e53f54d805336061
Ryan White <mrwhiterk@gmail.com>
2020-01-06 10:37:20 -0500

    updated the ui to use redux after a burger has been purchased to go back to home page

--- burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,7 +10,7 @@ import Spinner from '../../components/UI/Spinner/Spinner'
 import axios from '../../axios-orders'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 // import * as actionTypes from '../../store/actions/actionTypes'
-import * as burgerBuilderActions from '../../store/actions/index'
+import * as actions from '../../store/actions/index'
 
 class BurgerBuilder extends Component {
   state = {
@@ -42,6 +42,7 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
+    this.props.onInitPurchase()
     this.props.history.push('/checkout')
   }
 
@@ -114,10 +115,11 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     onIngredientAdded: ingName =>
-      dispatch(burgerBuilderActions.addIngredient(ingName)),
+      dispatch(actions.addIngredient(ingName)),
     onIngredientRemoved: ingName =>
-      dispatch(burgerBuilderActions.removeIngredient(ingName)),
-    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
+      dispatch(actions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(actions.initIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit())
   }
 }
 
--- burger-builder-basic/src/containers/Checkout/Checkout.js
+++ burger-builder-basic/src/containers/Checkout/Checkout.js
@@ -4,8 +4,11 @@ import { Route, Redirect } from 'react-router-dom'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
 import { connect } from 'react-redux'
+import * as actions from '../../store/actions/index'
 
 class Checkout extends Component {
+  
+
   checkoutCancelledHandler = () => {
     this.props.history.goBack()
   }
@@ -17,9 +20,12 @@ class Checkout extends Component {
   render() {
     let summary = <Redirect to="/" />
 
+    
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null; 
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -39,8 +45,10 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   }
 }
+
 
 export default connect(mapStateToProps)(Checkout)
