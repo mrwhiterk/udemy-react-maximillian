99d54cb2e3a4d28db5c28a758755fac0eba0a293
Ryan White <mrwhiterk@gmail.com>
2020-01-11 00:57:04 -0500

    setup signup route in for firebase; run REACT_APP_AUTH_API_KEY=[apikey] npm start to run

--- burger-builder-basic/src/containers/Auth/Auth.js
+++ burger-builder-basic/src/containers/Auth/Auth.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 
 import classes from './Auth.css'
+
+import * as actions from '../../store/actions/index'
 
 class Auth extends Component {
   state = {
@@ -63,11 +66,22 @@ class Auth extends Component {
       [controlName]: {
         ...this.state.controls[controlName],
         value: event.target.value,
-        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
         touched: true
       }
     }
     this.setState({ controls: updatedControls })
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    )
   }
 
   render() {
@@ -94,7 +108,7 @@ class Auth extends Component {
 
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -103,4 +117,10 @@ class Auth extends Component {
   }
 }
 
-export default Auth
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
