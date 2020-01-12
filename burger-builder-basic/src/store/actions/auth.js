6703dddaba7e0be94b7e41bb6d4d16d557c7aaaf
Ryan White <mrwhiterk@gmail.com>
2020-01-11 21:53:18 -0500

    got signIn working

--- burger-builder-basic/src/store/actions/auth.js
+++ burger-builder-basic/src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = error => {
   }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
     const authData = {
@@ -30,12 +30,17 @@ export const auth = (email, password) => {
       returnSecureToken: true
     }
 
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+      process.env.REACT_APP_AUTH_API_KEY
+    if (!isSignup) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        process.env.REACT_APP_AUTH_API_KEY
+    }
+
     axios
-      .post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-          process.env.REACT_APP_AUTH_API_KEY,
-        authData
-      )
+      .post(url, authData)
       .then(response => {
         console.log(response)
         dispatch(authSuccess(response.data))
