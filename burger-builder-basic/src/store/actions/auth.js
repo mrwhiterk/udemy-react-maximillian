502d712361dc9a527466fc4a7de40f55dfccc7e8
Ryan White <mrwhiterk@gmail.com>
2020-01-13 14:35:15 -0500

    got logout with token timeout working

--- burger-builder-basic/src/store/actions/auth.js
+++ burger-builder-basic/src/store/actions/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import * as actionTypes from './actionTypes'
 
+
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
@@ -19,6 +20,20 @@ export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
+  }
+}
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, expirationTime * 1000)
   }
 }
 
@@ -45,6 +60,7 @@ export const auth = (email, password, isSignup) => {
       .then(response => {
         console.log(response)
         dispatch(authSuccess(response.data.idToken, response.data.localId))
+        dispatch(checkAuthTimeout(response.data.expiresIn))
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error))
