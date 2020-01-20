3352d2a9d3dfbaecba04c355a850e23dade0da48
Ryan White <mrwhiterk@gmail.com>
2020-01-20 02:30:01 -0500

    moved checkValidity to its own shared file

--- burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-basic/src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import axios from '../../../axios-orders'
 import Input from '../../../components/UI/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
-import { updateObject } from '../../../shared/utility'
+import { updateObject, checkValidity } from '../../../shared/utility'
 
 class ContactData extends Component {
   state = {
@@ -120,32 +120,12 @@ class ContactData extends Component {
     this.props.onOrderBurger(order, this.props.token)
   }
 
-  checkValidity(value, rules) {
-    let isValid = true
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    return isValid
-  }
-
   inputChangedHandler = (e, inputIdentifier) => {
-    
-
     const updatedFormElement = updateObject(
       this.state.orderForm[inputIdentifier],
       {
         value: e.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           e.target.value,
           this.state.orderForm[inputIdentifier].validation
         ),
