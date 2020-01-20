adb8fdf7bc83331d5eed7387e1ecd9b02207bf00
Ryan White <mrwhiterk@gmail.com>
2020-01-20 17:44:31 -0500

    wrote test to test Navigation item has logout button on authenticated

--- burger-builder-basic/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder-basic/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -27,4 +27,12 @@ describe('<NavigationItems />', () => {
     })
     expect(wrapper.find(NavigationItem)).toHaveLength(3)
   })
+  
+  it('should render an exact logout button', () => {
+    wrapper.setProps({
+      isAuthenticated: true
+    })
+    
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
+  })
 })
