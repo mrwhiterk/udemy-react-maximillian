4ed3dd15fd025f9e60cc531a7e9ff621664cbef5
Ryan White <mrwhiterk@gmail.com>
2020-01-24 17:44:36 -0500

    built basic scaffolding for react app

---
+++ webpack_practice/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import { Link, Route } from 'react-router-dom'
+
+import Users from './containers/Users'
+import asyncComponent from './hoc/asyncComponent'
+
+const AsyncPizza = asyncComponent(() => {
+  return import('./containers/Pizza.js')
+})
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link> |
+          <Link to="/pizza">Pizza</Link> 
+
+        </div>
+        <div>
+          <Route path="/" exact Component={Users} />
+          <Route path="/pizza" exact Component={AsyncPizza} />
+
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
