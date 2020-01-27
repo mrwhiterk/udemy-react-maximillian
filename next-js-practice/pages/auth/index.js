ac1de760feaddcf457b1f896365922e9382efbc1
Ryan White <mrwhiterk@gmail.com>
2020-01-27 16:48:38 -0500

    used getInitialProps static method to pass props to components

--- next-js-practice/pages/auth/index.js
+++ next-js-practice/pages/auth/index.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 import User from '../../components/User'
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
   <div>
-    <h1>The Auth Index page</h1>
+    <h1>The Auth Index page - {props.appName}</h1>
     <User name="Max" age={38} />
   </div>
 )
+
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App (Auth)'})
+      }, 1000)
+    })
+    return promise
+  }
 
 export default authIndexPage
