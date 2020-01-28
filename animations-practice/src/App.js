c3a384ab60a8c1b93f143deb57a7491487c13ed9
Ryan White <mrwhiterk@gmail.com>
2020-01-27 20:26:18 -0500

    used transition component to toggle div

--- animations-practice/src/App.js
+++ animations-practice/src/App.js
@@ -5,9 +5,12 @@ import Modal from './components/Modal/Modal'
 import Backdrop from './components/Backdrop/Backdrop'
 import List from './components/List/List'
 
+import { Transition } from 'react-transition-group'
+
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,9 +25,44 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
-        <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exited' ? 0 : 1
+              }}
+            />
+          )}
+        </Transition>
+
+        {this.state.modalIsOpen ? (
+          <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
+        ) : null}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
